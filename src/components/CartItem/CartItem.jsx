7ea42e8f6050ae7177a784e2dfdac023ee1186a2
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-08-20 12:46:49 +0300

    cart item + cart page

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -10,7 +10,7 @@ function CartItem({ image, title, price, id }) {
     <div className={styles.cartItem}>
       <img src={image} alt={title} className={styles.img} />
       <p className={styles.itemTitle}>{title}</p>
-      <p className={styles.price}>{price}</p>
+      <p className={styles.price}>${price}</p>
       <button
         className={styles.button}
         onClick={() =>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,29 +1,41 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
 import CartItem from "../../components/CartItem/CartItem";
 import { CartContext } from "../../contexts/CartContext";
 import styles from "./Cart.module.css";
 
 function Cart() {
   const { cart } = useContext(CartContext);
-  console.log(cart);
-  return (
-    <div className={styles.cart}>
-      <p className={styles.title}>Your Cart</p>
-      <div className={styles.container}>
-        {cart?.map((item) => {
-          return (
-            <CartItem
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-            />
-          );
-        })}
+
+  if (cart.length === 0)
+    return (
+      <div className={styles.cart}>
+        <h2 className={styles.title}>Your cart is empty</h2>
+        <Link to="/">
+          <button className={styles.button}>Back to shop</button>
+        </Link>
       </div>
-    </div>
-  );
+    );
+  else
+    return (
+      <div className={styles.cart}>
+        <h2 className={styles.title}>Your cart</h2>
+        <div className={styles.container}>
+          {cart?.map((item) => {
+            return (
+              <CartItem
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
 }
 
 export default Cart;
